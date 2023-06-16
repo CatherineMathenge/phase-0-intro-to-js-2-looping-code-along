df7400dc8471bd4970955336638f84926be931b6
Catherine <catherine.mathenge@strathmore.edu>
2023-06-16 11:14:59 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+  function writeCards(names, event) {
+    const messages = [];
+    for (let name of names) {
+      messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
